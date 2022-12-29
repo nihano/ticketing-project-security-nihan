4bf4c2c5ae261e83faf82c16e3052f7a27983c34
nihano <nioztekin@gmail.com>
2022-12-29 23:00:47 +0300

    add security Filter Chain

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -3,12 +3,14 @@ package com.cydeo.config;
 import jdk.dynalink.linker.LinkerServices;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 import java.util.ArrayList;
@@ -27,6 +29,25 @@ public class SecurityConfig {
         userList.add(new User("ozzy", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER"))));
 
         //UserDetailsService is interface, returning the implementation class below
-        return new InMemoryUserDetailsManager();
+        return new InMemoryUserDetailsManager(userList);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .authorizeRequests() //every page needs to be authorized
+                .antMatchers( //no authentication for below
+                        "/",
+                        "/login",
+                        "/fragments/**",
+                        "/assets/**",
+                        "/images/**"
+                ).permitAll()
+                .anyRequest().authenticated()//rest needs to be authenticated
+                .and()
+                .httpBasic()//pop up page/form will remove later, and we use our login page
+                .and().build();
+
     }
 }
